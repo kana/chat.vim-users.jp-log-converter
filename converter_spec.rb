6757249103eedf1ac78b4eb24bfaf41fa7fb8343
kana <nicht@s8.xrea.com>
2009-05-30 01:42:17 +0900

    Fix 'should' clauses

--- converter_spec.rb
+++ converter_spec.rb
@@ -25,11 +25,11 @@ describe Converter, 'with a join message' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_JOIN_MESSAGE
   end
 
-  it 'should be type :join' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :join
   end
 
-  it 'should be nick "thinca"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'thinca'
   end
 
@@ -43,11 +43,11 @@ describe Converter, 'with a normal message without specials' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITHOUT_SPECIALS
   end
 
-  it 'should be type :msg' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :msg
   end
 
-  it 'should be nick "kana"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'kana'
   end
 
@@ -55,7 +55,7 @@ describe Converter, 'with a normal message without specials' do
     @parsed_line[:time].should == '03:17:42'
   end
 
-  it 'should be text "やることやった感"' do
+  it 'should have a valid text' do
     @parsed_line[:text].should == 'やることやった感'
   end
 end
@@ -65,11 +65,11 @@ describe Converter, 'with a normal message with an image link' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK
   end
 
-  it 'should be type :msg' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :msg
   end
 
-  it 'should be nick "kana"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'kana'
   end
 
@@ -77,7 +77,7 @@ describe Converter, 'with a normal message with an image link' do
     @parsed_line[:time].should == '03:22:04'
   end
 
-  it 'should be text "http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png"' do
+  it 'should have a valid text' do
     @parsed_line[:text].should == 'http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png'
   end
 end
@@ -87,11 +87,11 @@ describe Converter, 'with a normal message with a normal link' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK
   end
 
-  it 'should be type :msg' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :msg
   end
 
-  it 'should be nick "kana"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'kana'
   end
 
@@ -99,7 +99,7 @@ describe Converter, 'with a normal message with a normal link' do
     @parsed_line[:time].should == '03:17:35'
   end
 
-  it 'should be text "よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary"' do
+  it 'should have a valid text' do
     @parsed_line[:text].should == 'よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary'
   end
 end
@@ -109,11 +109,11 @@ describe Converter, 'with a normal message with a paste link' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_A_PASTE_LINK
   end
 
-  it 'should be type :msg' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :msg
   end
 
-  it 'should be nick "Shougo"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'Shougo'
   end
 
@@ -121,7 +121,7 @@ describe Converter, 'with a normal message with a paste link' do
     @parsed_line[:time].should == '14:28:59'
   end
 
-  it 'should be text "http://gist.github.com/119798"' do
+  it 'should have a valid text' do
     @parsed_line[:text].should == 'http://gist.github.com/119798'
   end
 end
@@ -131,11 +131,11 @@ describe Converter, 'with a nick message' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_NICK_MESSAGE
   end
 
-  it 'should be type :nick' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :nick
   end
 
-  it 'should be nick "ukstudio" -> "ukstudio_aw"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'ukstudio_aw'
     @parsed_line[:old_nick].should == 'ukstudio'
     @parsed_line[:new_nick].should == 'ukstudio_aw'
@@ -151,11 +151,11 @@ describe Converter, 'with a part message' do
     @parsed_line = Converter.new.parsed_line_from_raw_line RAW_PART_MESSAGE
   end
 
-  it 'should be type :part' do
+  it 'should have a valid type' do
     @parsed_line[:type].should == :part
   end
 
-  it 'should be nick "kana"' do
+  it 'should have a valid nick' do
     @parsed_line[:nick].should == 'kana'
   end
 
