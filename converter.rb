4574d975a1945e5df510f2ce7590b3f2e3496153
kana <nicht@s8.xrea.com>
2009-05-30 01:12:43 +0900

    Parse the type of a raw line.

--- converter.rb
+++ converter.rb
@@ -1,6 +1,26 @@
 #!/usr/bin/env ruby
 
 class Converter
+  def parsed_line_from_raw_line(raw_line)
+    parsed_line = {}
+
+    /^(\d\d:\d\d:\d\d) (.)(.*)$/ =~ raw_line
+    case Regexp.last_match 2
+    when nil then
+      raise RuntimeError, 'Invalid raw_line'
+    when '+' then
+      parsed_line[:type] = :join
+    when '!' then
+      parsed_line[:type] = :part
+    when '<' then
+      parsed_line[:type] = :msg
+    else
+      parsed_line[:type] = :nick
+    end
+
+    return parsed_line
+  end
+
   def main()
     0
   end
--- converter_spec.rb
+++ converter_spec.rb
@@ -5,35 +5,88 @@ require 'converter'
 
 
 
-describe Converter do
-  it 'should convert /join message nicely' do
+RAW_JOIN_MESSAGE = '01:09:27 + thinca (thinca!i=3b9c8a56@gateway/web/ajax/mibbit.com/x-7370eae4604f8456) to #Vim-users.jp@freenode'
+RAW_MSG_MESSAGE_WITHOUT_SPECIALS = '03:17:42 <#Vim-users.jp@freenode:kana> やることやった感'
+RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK = '03:22:04 <#Vim-users.jp@freenode:kana> http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png'
+RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK = '03:17:35 <#Vim-users.jp@freenode:kana> よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary'
+RAW_MSG_MESSAGE_WITH_A_PASTE_LINK = '14:28:59 <#Vim-users.jp@freenode:Shougo> http://gist.github.com/119798'
+RAW_NICK_MESSAGE = '09:34:25 ukstudio -> ukstudio_aw'
+RAW_PART_MESSAGE = '20:02:15 ! kana ("http://www.mibbit.com ajax IRC Client")'
+
+
+
+
+
+
+
+
+describe Converter, 'with a join message' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_JOIN_MESSAGE
   end
 
-  it 'should convert /part message nicely' do
+  it 'should be type :join' do
+    @parsed_line[:type].should == :join
+  end
+end
+
+describe Converter, 'with a normal message without specials' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITHOUT_SPECIALS
   end
 
-  it 'should convert /nick message nicely' do
+  it 'should be type :msg' do
+    @parsed_line[:type].should == :msg
+  end
+end
+
+describe Converter, 'with a normal message with an image link' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK
   end
 
-  it 'should convert a plain message nicely' do
+  it 'should be type :msg' do
+    @parsed_line[:type].should == :msg
+  end
+end
+
+describe Converter, 'with a normal message with a normal link' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK
   end
 
-  it 'should convert a message with an image link nicely' do
+  it 'should be type :msg' do
+    @parsed_line[:type].should == :msg
+  end
+end
+
+describe Converter, 'with a normal message with a paste link' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_MSG_MESSAGE_WITH_A_PASTE_LINK
   end
 
-  it 'should convert a message with multiple image links nicely' do
+  it 'should be type :msg' do
+    @parsed_line[:type].should == :msg
+  end
+end
+
+describe Converter, 'with a nick message' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_NICK_MESSAGE
   end
 
-  it 'should convert a message with an paste-service link nicely' do
+  it 'should be type :nick' do
+    @parsed_line[:type].should == :nick
+  end
+end
+
+describe Converter, 'with a part message' do
+  before do
+    @parsed_line = Converter.new.parsed_line_from_raw_line RAW_PART_MESSAGE
   end
 
-  it 'should convert a message with multiple paste-service links nicely' do
-  end
-
-  it 'should convert a message with multiple kinds of special links nicely' do
-  end
-
-  it 'should convert multiple messages nicely' do
+  it 'should be type :part' do
+    @parsed_line[:type].should == :part
   end
 end
 
