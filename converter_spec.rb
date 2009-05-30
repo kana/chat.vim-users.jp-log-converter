1d8e6a7f92a271dd8b5b15180619596b911a2374
kana <nicht@s8.xrea.com>
2009-05-30 13:25:53 +0900

    Refactor - Naming of constants

--- converter_spec.rb
+++ converter_spec.rb
@@ -5,22 +5,22 @@ require 'converter'
 require 'stringio'
 
 
-RAW_JOIN_MESSAGE = '01:09:27 + thinca (thinca!i=3b9c8a56@gateway/web/ajax/mibbit.com/x-7370eae4604f8456) to #Vim-users.jp@freenode'
-RAW_MSG_MESSAGE_WITHOUT_SPECIALS = '03:17:42 <#Vim-users.jp@freenode:kana> やることやった感'
-RAW_MSG_MESSAGE_WITHOUT_SPECIALS2 = '14:00:37 >#Vim-users.jp@freenode:from_kyushu< gyazoみたく簡単にできれば良いのだけども'
-RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK = '03:22:04 <#Vim-users.jp@freenode:kana> http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png'
-RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK = '03:17:35 <#Vim-users.jp@freenode:kana> よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary'
-RAW_MSG_MESSAGE_WITH_A_PASTE_LINK = '14:28:59 <#Vim-users.jp@freenode:Shougo> http://gist.github.com/119798'
-RAW_NICK_MESSAGE = '09:34:25 ukstudio -> ukstudio_aw'
-RAW_PART_MESSAGE = '20:02:15 ! kana ("http://www.mibbit.com ajax IRC Client")'
-RAW_TOPIC_MESSAGE = '13:45:40 Topic of channel #Vim-users.jp@freenode by from_kyushu: ログサーバを一時的に復帰 http://chat.vim-users.jp/ for true vim users and not true vim users.'
+RLINE_JOIN = '01:09:27 + thinca (thinca!i=3b9c8a56@gateway/web/ajax/mibbit.com/x-7370eae4604f8456) to #Vim-users.jp@freenode'
+RLINE_MSG_NORMAL = '03:17:42 <#Vim-users.jp@freenode:kana> やることやった感'
+RLINE_MSG_NORMAL2 = '14:00:37 >#Vim-users.jp@freenode:from_kyushu< gyazoみたく簡単にできれば良いのだけども'
+RLINE_MSG_LINK_IMAGE = '03:22:04 <#Vim-users.jp@freenode:kana> http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png'
+RLINE_MSG_LINK_NORMAL = '03:17:35 <#Vim-users.jp@freenode:kana> よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary'
+RLINE_MSG_LINK_PASTE = '14:28:59 <#Vim-users.jp@freenode:Shougo> http://gist.github.com/119798'
+RLINE_NICK = '09:34:25 ukstudio -> ukstudio_aw'
+RLINE_PART = '20:02:15 ! kana ("http://www.mibbit.com ajax IRC Client")'
+RLINE_TOPIC = '13:45:40 Topic of channel #Vim-users.jp@freenode by from_kyushu: ログサーバを一時的に復帰 http://chat.vim-users.jp/ for true vim users and not true vim users.'
 
 
 
 
 describe Converter, 'parsing a join message' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_JOIN_MESSAGE
+    @pline = Converter.new.pline_from_rline RLINE_JOIN
   end
 
   it 'should have a valid type' do
@@ -42,7 +42,7 @@ end
 describe Converter, 'converting a join message' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_JOIN_MESSAGE
+    pline = c.pline_from_rline RLINE_JOIN
     cline = c.cline_of_join_from_pline pline, 1
     cline.should == '<li id="L1" class="join"><span class="time">01:09:27</span> <span class="nick">thinca</span> <span class="text">has joined</span></li>'
   end
@@ -53,7 +53,7 @@ end
 
 describe Converter, 'parsing a normal message without specials' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_MSG_MESSAGE_WITHOUT_SPECIALS
+    @pline = Converter.new.pline_from_rline RLINE_MSG_NORMAL
   end
 
   it 'should have a valid type' do
@@ -79,7 +79,7 @@ end
 describe Converter, 'converting a normal message without specials' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_MSG_MESSAGE_WITHOUT_SPECIALS
+    pline = c.pline_from_rline RLINE_MSG_NORMAL
     cline = c.cline_of_msg_from_pline pline, 5
     cline.should == '<li id="L5" class="msg"><span class="time">03:17:42</span> <span class="nick">kana</span> <span class="text">やることやった感</span></li>'
   end
@@ -90,7 +90,7 @@ end
 
 describe Converter, 'parsing another normal message without specials' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_MSG_MESSAGE_WITHOUT_SPECIALS2
+    @pline = Converter.new.pline_from_rline RLINE_MSG_NORMAL2
   end
 
   it 'should have a valid type' do
@@ -116,7 +116,7 @@ end
 describe Converter, 'converting another normal message without specials' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_MSG_MESSAGE_WITHOUT_SPECIALS2
+    pline = c.pline_from_rline RLINE_MSG_NORMAL2
     cline = c.cline_of_msg_from_pline pline, 6
     cline.should == '<li id="L6" class="msg"><span class="time">14:00:37</span> <span class="nick">from_kyushu</span> <span class="text">gyazoみたく簡単にできれば良いのだけども</span></li>'
   end
@@ -127,7 +127,7 @@ end
 
 describe Converter, 'parsing a normal message with an image link' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK
+    @pline = Converter.new.pline_from_rline RLINE_MSG_LINK_IMAGE
   end
 
   it 'should have a valid type' do
@@ -153,7 +153,7 @@ end
 describe Converter, 'converting a normal message with an image link' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK
+    pline = c.pline_from_rline RLINE_MSG_LINK_IMAGE
     cline = c.cline_of_msg_from_pline pline, 7
     cline.should == '<li id="L7" class="msg"><span class="time">03:22:04</span> <span class="nick">kana</span> <span class="text">http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png</span></li>'
   end
@@ -164,7 +164,7 @@ end
 
 describe Converter, 'parsing a normal message with a normal link' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK
+    @pline = Converter.new.pline_from_rline RLINE_MSG_LINK_NORMAL
   end
 
   it 'should have a valid type' do
@@ -190,7 +190,7 @@ end
 describe Converter, 'converting a normal message with a normal link' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK
+    pline = c.pline_from_rline RLINE_MSG_LINK_NORMAL
     cline = c.cline_of_msg_from_pline pline, 8
     cline.should == '<li id="L8" class="msg"><span class="time">03:17:35</span> <span class="nick">kana</span> <span class="text">よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary</span></li>'
   end
@@ -201,7 +201,7 @@ end
 
 describe Converter, 'parsing a normal message with a paste link' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_MSG_MESSAGE_WITH_A_PASTE_LINK
+    @pline = Converter.new.pline_from_rline RLINE_MSG_LINK_PASTE
   end
 
   it 'should have a valid type' do
@@ -227,7 +227,7 @@ end
 describe Converter, 'converting a normal message with a paste link' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_MSG_MESSAGE_WITH_A_PASTE_LINK
+    pline = c.pline_from_rline RLINE_MSG_LINK_PASTE
     cline = c.cline_of_msg_from_pline pline, 9
     cline.should == '<li id="L9" class="msg"><span class="time">14:28:59</span> <span class="nick">Shougo</span> <span class="text">http://gist.github.com/119798</span></li>'
   end
@@ -238,7 +238,7 @@ end
 
 describe Converter, 'parsing a nick message' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_NICK_MESSAGE
+    @pline = Converter.new.pline_from_rline RLINE_NICK
   end
 
   it 'should have a valid type' do
@@ -262,7 +262,7 @@ end
 describe Converter, 'converting a nick message' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_NICK_MESSAGE
+    pline = c.pline_from_rline RLINE_NICK
     cline = c.cline_of_nick_from_pline pline, 2
     cline.should == '<li id="L2" class="nick"><span class="time">09:34:25</span> <span class="text"><span class="old-nick">ukstudio</span> is now as known as <span class="new-nick">ukstudio_aw</span></span></li>'
   end
@@ -273,7 +273,7 @@ end
 
 describe Converter, 'parsing a part message' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_PART_MESSAGE
+    @pline = Converter.new.pline_from_rline RLINE_PART
   end
 
   it 'should have a valid type' do
@@ -295,7 +295,7 @@ end
 describe Converter, 'converting a part message' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_PART_MESSAGE
+    pline = c.pline_from_rline RLINE_PART
     cline = c.cline_of_part_from_pline pline, 3
     cline.should == '<li id="L3" class="part"><span class="time">20:02:15</span> <span class="nick">kana</span> <span class="text">has left</span></li>'
   end
@@ -306,7 +306,7 @@ end
 
 describe Converter, 'parsing a topic message' do  #{{{1
   before do
-    @pline = Converter.new.pline_from_rline RAW_TOPIC_MESSAGE
+    @pline = Converter.new.pline_from_rline RLINE_TOPIC
   end
 
   it 'should have a valid type' do
@@ -332,7 +332,7 @@ end
 describe Converter, 'converting a topic message' do  #{{{1
   it 'should convert the line nicely' do
     c = Converter.new
-    pline = c.pline_from_rline RAW_TOPIC_MESSAGE
+    pline = c.pline_from_rline RLINE_TOPIC
     cline = c.cline_of_topic_from_pline pline, 4
     cline.should == '<li id="L4" class="topic"><span class="time">13:45:40</span> <span class="nick">from_kyushu</span> <span class="text">sets topic: <span class="topic">ログサーバを一時的に復帰 http://chat.vim-users.jp/ for true vim users and not true vim users.</span></span></li>'
   end
@@ -389,15 +389,15 @@ describe Converter, 'main with a valid argument' do  #{{{1
   it 'should return 0' do
     original_stdin = $stdin
       $stdin = StringIO.new [
-        RAW_JOIN_MESSAGE,
-        RAW_MSG_MESSAGE_WITHOUT_SPECIALS,
-        RAW_MSG_MESSAGE_WITHOUT_SPECIALS2,
-        RAW_MSG_MESSAGE_WITH_AN_IMAGE_LINK,
-        RAW_MSG_MESSAGE_WITH_A_NORMAL_LINK,
-        RAW_MSG_MESSAGE_WITH_A_PASTE_LINK,
-        RAW_NICK_MESSAGE,
-        RAW_PART_MESSAGE,
-        RAW_TOPIC_MESSAGE,
+        RLINE_JOIN,
+        RLINE_MSG_NORMAL,
+        RLINE_MSG_NORMAL2,
+        RLINE_MSG_LINK_IMAGE,
+        RLINE_MSG_LINK_NORMAL,
+        RLINE_MSG_LINK_PASTE,
+        RLINE_NICK,
+        RLINE_PART,
+        RLINE_TOPIC,
         ''
       ].join("\n")
       Converter.new.main(['2009-05-30']).should == 0
