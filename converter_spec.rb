049acc6fcf340890d1e9aeb8b231c88f0a60ed09
kana <nicht@s8.xrea.com>
2009-05-30 14:32:57 +0900

    Add tests to convert multiple links

--- converter_spec.rb
+++ converter_spec.rb
@@ -7,6 +7,7 @@ require 'stringio'
 
 RLINE_INVALID = 'Invalid format'
 RLINE_JOIN = '01:09:27 + thinca (thinca!i=3b9c8a56@gateway/web/ajax/mibbit.com/x-7370eae4604f8456) to #Vim-users.jp@freenode'
+RLINE_MSG_LINK_ALL_TYPES = '00:01:02 <#Vim-users.jp@freenode:kana> foo http://whileimautomaton.net/2009/05/29/02/37/54/diary bar http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png baz http://gist.github.com/119798 hehehe'
 RLINE_MSG_LINK_IMAGE = '03:22:04 <#Vim-users.jp@freenode:kana> http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png'
 RLINE_MSG_LINK_NORMAL = '03:17:35 <#Vim-users.jp@freenode:kana> よし寝る http://whileimautomaton.net/2009/05/29/02/37/54/diary'
 RLINE_MSG_LINK_PASTE = '14:28:59 <#Vim-users.jp@freenode:Shougo> http://gist.github.com/119798'
@@ -232,6 +233,43 @@ describe Converter, 'converting a normal message with a paste link' do  #{{{1
     pline = c.pline_from_rline RLINE_MSG_LINK_PASTE
     cline = c.cline_of_msg_from_pline pline, 9
     cline.should == '<li id="L9" class="msg"><span class="time">14:28:59</span> <span class="nick">Shougo</span> <span class="text">http://gist.github.com/119798</span></li>'
+  end
+end
+
+
+
+
+describe Converter, 'parsing a normal message with multiple links' do  #{{{1
+  before do
+    @pline = Converter.new.pline_from_rline RLINE_MSG_LINK_ALL_TYPES
+  end
+
+  it 'should have a valid type' do
+    @pline[:type].should == :msg
+  end
+
+  it 'should have a valid nick' do
+    @pline[:nick].should == 'kana'
+  end
+
+  it 'should have a valid time' do
+    @pline[:time].should == '00:01:02'
+  end
+
+  it 'should have a valid text' do
+    @pline[:text].should == 'foo http://whileimautomaton.net/2009/05/29/02/37/54/diary bar http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png baz http://gist.github.com/119798 hehehe'
+  end
+end
+
+
+
+
+describe Converter, 'converting a normal message with multiple links' do  #{{{1
+  it 'should convert the line nicely' do
+    c = Converter.new
+    pline = c.pline_from_rline RLINE_MSG_LINK_ALL_TYPES
+    cline = c.cline_of_msg_from_pline pline, 11
+    cline.should == '<li id="L11" class="msg"><span class="time">00:01:02</span> <span class="nick">kana</span> <span class="text">foo http://whileimautomaton.net/2009/05/29/02/37/54/diary bar http://gyazo.com/af8f793b7371a721bbb06059b8d3d5fe.png baz http://gist.github.com/119798 hehehe</span></li>'
   end
 end
 
